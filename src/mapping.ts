8ccfec738d70ebcff6e36b2b0ecf18d12901fadd
Greg <gjsyme@gmail.com>
2021-09-22 17:54:18 -0500

    Created initial through v0.0.4 developer_dao graph
    Initial setup was taken from the guide on The Graph
    Further iterations (v0.0.2 and v0.0.3) moved to use Developer data vice
    the example entities
    v0.0.4 solved getting the name and the token ID as we are accustomed to
    seeing them

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Entity } from "@graphprotocol/graph-ts"
 import {
   Contract,
   Approval,
@@ -6,71 +6,33 @@ import {
   OwnershipTransferred,
   Transfer
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Developer } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getClothing(...)
-  // - contract.getIndustry(...)
-  // - contract.getLanguage(...)
-  // - contract.getLocation(...)
-  // - contract.getMind(...)
-  // - contract.getOS(...)
-  // - contract.getTextEditor(...)
-  // - contract.getVibe(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
-}
+// stub like others
+export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let developer = Developer.load(event.params.tokenId.toString())
+  
+  if(!developer){
+    developer = new Developer(event.params.tokenId.toString())
+    // populate values from contract
+    let contract = Contract.bind(event.address)
+    developer.name = `Dev #${event.params.tokenId}`
+    developer.clothing = contract.getClothing(event.params.tokenId)
+    developer.industry = contract.getIndustry(event.params.tokenId)
+    developer.language = contract.getLanguage(event.params.tokenId)
+    developer.location = contract.getLocation(event.params.tokenId)
+    developer.mind = contract.getMind(event.params.tokenId)
+    developer.os = contract.getOS(event.params.tokenId)
+    developer.textEditor = contract.getTextEditor(event.params.tokenId)
+    developer.vibe = contract.getVibe(event.params.tokenId)
+  }
+
+  developer.owner = event.params.to
+  developer.save()
+}
